83e79351cbb94ff1ba59ca764956d5f51c97820c
Sochit Sovattana <sovatsovag@gmail.com>
2024-05-07 20:20:23 +0700

    new sign in page and modify footer and header

--- javascript/footer-injection.js
+++ javascript/footer-injection.js
@@ -4,18 +4,23 @@ footer.innerHTML=  `
 
 
 <div class="feature-container">
-        <div class="featured-on-homepage" >
-            <div class="feature">
-                <h4>Featured</h4>
+    <div class="featured-on-homepage" >
+        <div class="feature">
+            <button type="button" class="collapsible" href="..." >Featured</button>
+            <div class="content">
                 <ul class="sub-feature">
-                    <li><a>Air Force 1</a></li>
-                    <li><a>Jordan 1</a></li>
-                    <li><a>Metcon</a></li>
-                    <li><a>Air Max 270</a></li>
+                <li><a>Air Force 1</a></li>
+                <li><a>Jordan 1</a></li>
+                <li><a>Metcon</a></li>
+                <li><a>Air Max 270</a></li>
                 </ul>
             </div>
-            <div class="shoes">
-                <h4>Shoes</h4>
+            
+        </div>
+        <div class="shoes">
+            <button type="button" class="collapsible" href="..." >Shoes</button>
+            <div class="content">
+                
                 <ul class="sub-shoes">
                     <li><a>All Shoes</a></li>
                     <li><a>Jordan Shoes</a></li>
@@ -23,17 +28,21 @@ footer.innerHTML=  `
                     <li><a>Basketball Shoes</a></li>
                 </ul>
             </div>
-            <div class="clothing">
-                <h4>Clothing</h4>
-                <ul class="sub-clothing">
+        </div>
+        <div class="clothing">
+            <button type="button" class="collapsible" href="..." >Clothing</button>
+            <div class="content">
+               <ul class="sub-clothing">
                     <li><a>All Clothing</a></li>
                     <li><a>Tops & T-Shirts</a></li>
                     <li>Shorts<a></a></li>
                     <li><a>Hoodies & Pullovers</a></li>
-                </ul>
+                </ul> 
             </div>
-            <div Class="kids">
-                <h4>Kids</h4>
+        </div>
+        <div Class="kids">
+            <button type="button" class="collapsible" href="..." >Kids</button>
+            <div class="content">
                 <ul class="sub-kids">
                     <li><a>Infant & Toddler Shoes</a></li>
                     <li><a>Kids Shoes</a></li>
@@ -41,28 +50,27 @@ footer.innerHTML=  `
                     <li><a>Kids Running Shoes</a></li>
                 </ul>
             </div>
+            
         </div>
     </div>
+</div>
 
 
-
-
-
-<!-- footer -->
-    <footer class="footer-container" >
-        <div class="footer-bottom" >
-            <div class="footer-list" >
-               <ul class="footer-info">
-                    <li><a class="footer-info-list" href="...">RESOURCES</a></li>
-                    <li><a  class="footer-info-list" href="...">GIFT CARDS</a></li>  
-                    <li><a  class="footer-info-list" href="...">FIND A STORE</a></li>  
-                    <li><a  class="footer-info-list" href="...">BECOME A MEMER</a></li>   
-                    <li><a  class="footer-info-list" href="...">NIKE X NBA</a></li>
-                    <li><a  class="footer-info-list" href="...">NIKE JOURNAL</a></li>             
-                    <li><a  class="footer-info-list" href="...">Rite Feedback</a></li>
-               </ul>
-                <ul class="footer-info-help">
-                    <li><a class="footer-info-help-list-head" href="..." id="footer-info-help-list-head">HELP</a></li>
+<footer class="footer-container" >
+    <div class="footer-bottom" >
+        <div class="footer-list" >
+           <ul class="footer-info">
+                <li><a class="footer-info-list" href="...">RESOURCES</a></li>
+                <li><a  class="footer-info-list" href="...">GIFT CARDS</a></li>  
+                <li><a  class="footer-info-list" href="...">FIND A STORE</a></li>  
+                <li><a  class="footer-info-list" href="...">BECOME A MEMER</a></li>   
+                <li><a  class="footer-info-list" href="...">NIKE X NBA</a></li>
+                <li><a  class="footer-info-list" href="...">NIKE JOURNAL</a></li>             
+                <li><a  class="footer-info-list" href="...">Site Feedback</a></li>
+           </ul>
+            <ul class="footer-info-help">
+                <button type="button" class="collapsible" href="..." id="footer-info-help-list-head">HELP</button>
+                <div class="content">
                     <li><a class="footer-info-help-list" href="...">Get Help</a></li>
                     <li><a class="footer-info-help-list" href="...">Order Status</a></li>
                     <li><a class="footer-info-help-list" href="...">Shipping and Delivery</a></li>
@@ -71,67 +79,72 @@ footer.innerHTML=  `
                     <li><a class="footer-info-help-list" href="...">Payment Options</a></li>
                     <li><a class="footer-info-help-list" href="...">Gift Card Balance</a></li>
                     <li><a class="footer-info-help-list" href="...">Contact Us</a></li>
-                </ul>
-                <ul class="footer-info-company">
-                    <li><a class="footer-info-company-list-head" href=".." id="footer-info-company-list-head">COMPANY</a></li>
-                    <li><a class="footer-info-help-list" href="..">About Nike</a></li>
-                    <li><a class="footer-info-help-list" href="..">News</a></li>
-                    <li><a class="footer-info-help-list" href="..">Careers</a></li>
-                    <li><a class="footer-info-help-list" href="..">Investors</a></li>
-                    <li><a class="footer-info-help-list" href="..">Purpose</a></li>
-                    <li><a class="footer-info-help-list" href="..">Sustainability</a></li>
-                </ul>
-                <ul class="footer-info-company">
-                    <li><a class="footer-info-promotion-list-head" href="..." id="footer-info-promotion-list-head">PROMOTION & DISCOUNTS</a></li>
-                    <li><a class="footer-info-help-list" href="...">Student</a></li>
-                    <li><a class="footer-info-help-list" href="...">Military</a></li>
-                    <li><a class="footer-info-help-list" href="...">Teacher</a></li>
-                    <li><a class="footer-info-help-list" href="...">First Responders & Medical</a></li>
-                    <li><a class="footer-info-help-list" href="...">Professionals</a></li>
-                    <li><a class="footer-info-help-list" href="...">Birthday</a></li>
-                </ul>
-                <div class="social-media-logo">
-                    <div class="twitter">
-                        <a><img src="../Nike-Image/social-media-logo/5305170_bird_social media_social network_tweet_twitter_icon.svg"></a>
-                    </div>
-                    <div class="facebook">
-                        <a><img src="../Nike-Image/social-media-logo/104498_facebook_icon.svg"></a>
-                    </div>
-                    <div class="youtube">
-                        <a><img src="../Nike-Image/social-media-logo/104482_youtube_icon.svg"></a>
-                    </div>
-                    <div class="instagram">
-                        <a><img src="../Nike-Image/social-media-logo/1161953_instagram_icon.svg"></a>
-                    </div>
                 </div>
-            </div>
-            <div class="very-bot-of-footer">
-                <div class="col">
-                    <div class="location">
-                        <div >
-                            <ul id="location-and-usa">
-                                <li><img src="../Nike-Image/social-media-logo/1071016_location_gps_pin marker_icon.svg" height="20"><a id="USA">United States</a></li>
-                                <li><a id="all-right-reserved" >© 2024 Nike, Inc. All Rights Reserved</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
+            </ul>
+            <ul class="footer-info-company">
+                <button type="button" class="collapsible" href="..." id="footer-info-company-list-head">COMPANY</button>
+                <div class="content">
+                <li><a class="footer-info-help-list" href="..">About Nike</a></li>
+                <li><a class="footer-info-help-list" href="..">News</a></li>
+                <li><a class="footer-info-help-list" href="..">Careers</a></li>
+                <li><a class="footer-info-help-list" href="..">Investors</a></li>
+                <li><a class="footer-info-help-list" href="..">Purpose</a></li>
+                <li><a class="footer-info-help-list" href="..">Sustainability</a></li>
                 </div>
-                <div class="col">
-                    <ul class="very-bot-of-footer-guide">
-                        <li><a href=".." class="very-bot-of-footer-guide-list">Guide</a></li>
-                        <li><a href=".." class="very-bot-of-footer-guide-list">Term of Sales</a></li>
-                        <li><a href=".." class="very-bot-of-footer-guide-list">Term of Use</a></li>
-                        <li><a href=".." class="very-bot-of-footer-guide-list">Nike Privacy Provicy</a></li>
-                        <li><a href=".." class="very-bot-of-footer-guide-list">Your Privacy Choices</a></li>                        
-                        <li><a href=".." class="very-bot-of-footer-guide-list-1">CA Supply Chain Act</a></li>
-                    </ul>
-
+            </ul>
+            <ul class="footer-info-company">
+                <button type="button" class="collapsible" href="..." id="footer-info-promotion-list-head">PROMOTION & DISCOUNTS</button>
+                <div class="content">
+                <li><a class="footer-info-help-list" href="...">Student</a></li>
+                <li><a class="footer-info-help-list" href="...">Military</a></li>
+                <li><a class="footer-info-help-list" href="...">Teacher</a></li>
+                <li><a class="footer-info-help-list" href="...">First Responders & Medical</a></li>
+                <li><a class="footer-info-help-list" href="...">Professionals</a></li>
+                <li><a class="footer-info-help-list" href="...">Birthday</a></li>
                 </div>
-                
+            </ul>
+            <div class="social-media-logo">
+                <div class="twitter">
+                    <a href="https://twitter.com/Nike"><img src="../Nike-Image/social-media-logo/5305170_bird_social media_social network_tweet_twitter_icon.svg"></a>
+                </div>
+                <div class="facebook">
+                    <a href="https://www.facebook.com/nike"><img  src="../Nike-Image/social-media-logo/104498_facebook_icon.svg"></a>
+                </div>
+                <div class="youtube">
+                    <a href="https://www.youtube.com/user/nike"><img src="../Nike-Image/social-media-logo/104482_youtube_icon.svg"></a>
+                </div>
+                <div class="instagram">
+                    <a href="https://www.instagram.com/nike/"><img src="../Nike-Image/social-media-logo/1161953_instagram_icon.svg"></a>
+                </div>
             </div>
         </div>
-    </footer>
+        <div class="very-bot-of-footer">
+            <div class="col">
+                <div class="location">
+                    <div >
+                        <ul id="location-and-usa">
+                            <li><img src="../Nike-Image/social-media-logo/1071016_location_gps_pin marker_icon.svg" height="20"><a id="USA">United States</a></li>
+                            <li><a id="all-right-reserved" >© 2024 Nike, Inc. All Rights Reserved</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col">
+                <ul class="very-bot-of-footer-guide">
+                    <li><a href=".." class="very-bot-of-footer-guide-list">Guide</a></li>
+                    <li><a href=".." class="very-bot-of-footer-guide-list">Term of Sales</a></li>
+                    <li><a href=".." class="very-bot-of-footer-guide-list">Term of Use</a></li>
+                    <li><a href=".." class="very-bot-of-footer-guide-list">Nike Privacy Provicy</a></li>
+                    <li><a href=".." class="very-bot-of-footer-guide-list">Your Privacy Choices</a></li>                        
+                    <li><a href=".." class="very-bot-of-footer-guide-list-1">CA Supply Chain Act</a></li>
+                </ul>
+
+            </div>
+            
+        </div>
+    </div>
+</footer>
 
 
 `
